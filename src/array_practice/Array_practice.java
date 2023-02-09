342498f12be3c6b58aaa12e90fb6b1f9f0338809
Okafati2 <odeh.kafati2@gmail.com>
2023-02-08 21:05:49 -0600

    reading the string and removing the spaces

--- src/array_practice/Array_practice.java
+++ src/array_practice/Array_practice.java
@@ -1,11 +1,27 @@
 package array_practice;
 
+import java.lang.reflect.Array;
+import java.util.Scanner;
+
 public class Array_practice {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		// Welcome the user to the program
+		System.out.println("This program takes a string and returns the amount of letters in said string");
+		System.out.print("Enter said string: ");
+		Scanner sc = new Scanner (System.in);
+		String scanned = sc.nextLine();
+		//Replace all spaces
+		String scanned1 = scanned.replaceAll("\\s","");
+		//Creates the array of letters (using .toCharArray())
+		char[] ch = scanned1.toCharArray();
+		//Print the array elements
+		for (char c : ch)
+		{
+			System.out.println(c);
+		}
 
-		System.out.println("Hi!");
+
 	}
-
 }
